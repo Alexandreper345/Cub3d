e1c93a3c66e26220d4037f4d427089fb05c82578
alexandreper345 <asilvaperoba@gmail.com>
2025-11-09 23:36:38 -0300

    done flood fill

--- src/parsing/flood_fill.c
+++ src/parsing/flood_fill.c
@@ -12,13 +12,25 @@
 
 #include "../../include/cub3d.h"
 
-int	flood_fill(char **matriz)
+int	flood_fill(char **dup_map, int x, int y)
 {
-
+	if (x < 0 || y < 0 || !map[y] || map[y][x] == '\0')
+		return (EXIT_FAILURE);
+	if (dup_map[x][y] == 'x' || dup_map[x][y] == ' ')
+		dup_map[x][y] == '-';
+	if (dup_map[x][y] == '1')
+		return (EXIT_FAILURE);
+	if(dup_map[x][y] == '0' || dup_map[x][y] == 'N' ||
+		dup_map[x][y] == 'S' || dup_map[x][y] == 'E' || dup_map[x][y] == 'W')
+	{
+		dup_map[x][y] == '-';
+		return (EXIT_SUCCESS);
+	}
+	if (dup_map[x][y] == '-')
+		return (EXIT_FAILURE);
+	return (flood_fill(dup_map, x + 1, y) && flood_fill(dup_map, x - 1, y) 
+			flood_fill(dup_map, x, y + 1) &&  flood_fill(dup_map, x, y - 1));
 }
-
-
-
 
 char	**copy_matriz(int height, int width)
 {
@@ -43,13 +55,38 @@ char	**copy_matriz(int height, int width)
 	return (copy_matriz);
 }
 
+char	**expand_map(char **dup_map, char **map, int width, int height)
+{
+	int	i;
+	int	j;
 
+	i = -1;
+	while(++i < height)
+	{
+		j = 0;
+		while(dup_map[i][j] && dup_map[i][j] != '\n')
+		{
+			dup_map[i + 1][j + 1] = map[i][j];
+			j++;
+		}
+	}
+	dup_map[height + 2] = NULL;
+	return (dup_map);
+}
 
 int	init_process_flood(t_map *map)
 {
 	int	width;
 	int	height;
-
+	char	**dup_map;
+	
 	width = get_width_map(map->matriz);
 	height = get_height_map(map->matriz);
+	dup_map = copy_matriz(height, width);
+	if (!dup_map)
+		return (EXIT_FAILURE);
+	dup_map = expand_map(dup_map , map, height, width);
+	if (flood_fill(dup_map, width, height))
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
